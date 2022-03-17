91d97d5376aea6e0a624cded706a42a274818114
Kayo Costa <kayo220@users.noreply.github.com>
2022-03-17 20:58:41 -0300

    using config module for database integration

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3000);
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT);
 }
 bootstrap();
